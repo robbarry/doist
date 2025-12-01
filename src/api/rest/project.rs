5c9f447c612ff136b45feff66c0d90a7df5ba54c
Dominykas Djačenko <chaosteil@gmail.com>
2025-11-30 19:31:11 -0800

    Clarify error message for doist edit (#143)

--- src/api/rest/project.rs
+++ src/api/rest/project.rs
@@ -102,19 +102,15 @@ impl PartialOrd for Project {
 /// Taken from the [Developer Documentation](https://developer.todoist.com/api/v1/#projects).
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Ord, PartialOrd, Clone)]
 #[serde(rename_all = "lowercase")]
+#[derive(Default)]
 pub enum ViewStyle {
     /// Project as list view (default).
+    #[default]
     List,
     /// Project as board view.
     Board,
     /// Project as calendar view.
     Calendar,
-}
-
-impl Default for ViewStyle {
-    fn default() -> Self {
-        Self::List
-    }
 }
 
 impl Treeable for Project {
